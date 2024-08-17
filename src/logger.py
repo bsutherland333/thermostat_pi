a7a1a6c8092a41c085713b02f9e1860f9f033bfb
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-08-16 22:46:48 -0600

    Added dates to logger filename

--- src/logger.py
+++ src/logger.py
@@ -1,34 +1,39 @@
 import csv
+import os
 import string
 from datetime import datetime
 
 
 class Logger:
-    def __init__(self, filename: string, header: list):
+    def __init__(self, filename: string, filepath: string, header: list):
         self.filename = filename
+        self.filepath = filepath
         self.header = header
         self.num_columns = len(header)
-
-        # Create the CSV file if it doesn't exist
-        with open(self.filename, mode='a', newline='') as file:
-            writer = csv.writer(file)
-            if file.tell() == 0:
-                writer.writerow(['Time', 'Epoch Time (s)', *header])
 
     def log(self, data: list):
         # Check if the data has the correct number of columns
         if len(data) != self.num_columns:
-            raise ValueError('Data does not have the correct number of columns')
+            raise ValueError("Data does not have the correct number of columns")
 
-        # Get the current time
-        current_time = datetime.now().strftime('%Y_%m_%d:%H_%M_%S')
+        # Get the current date and time
+        current_date = datetime.now().strftime("%Y_%m_%d")
+        current_time = datetime.now().strftime("%H_%M_%S")
 
         # Get the current time in epoch format
         current_time_epoch = datetime.now().timestamp()
 
+        # Create the full filepath
+        appended_filename = f"{self.filename}_{current_date}.csv"
+        full_filepath = os.path.join(self.filepath, appended_filename)
+
         # Open the CSV file in append mode
-        with open(self.filename, mode='a', newline='') as file:
+        with open(full_filepath, mode="a", newline="") as file:
             writer = csv.writer(file)
+
+            # Write the header if the file is empty
+            if file.tell() == 0:
+                writer.writerow(["Time", "Epoch Time (s)", *header])
 
             # Write the current time and temperature to the file
             writer.writerow([current_time, current_time_epoch, *data])
--- src/main.py
+++ src/main.py
@@ -1,27 +1,24 @@
 import time
 
 from temp_sensor import TempSensor
-from relay_control import RelayControl, RelayType
 from logger import Logger
 
+LOG_PATH = "/home/brandon/thermostat_logs/"
 
 def main():
     sensor = TempSensor()
-    fan = RelayControl(RelayType.FAN)
-    cool = RelayControl(RelayType.COOL)
-    heat = RelayControl(RelayType.HEAT)
-    temp_log = Logger('/home/brandon/logs/temperature_log.csv', ['Temperature (C)', 'Temperature (F)'])
-    status_log = Logger('/home/brandon/logs/status_log.csv', ['Message'])
+    temp_log = Logger("temperature", LOG_PATH, ["Temperature (C)", "Temperature (F)"])
+    status_log = Logger("status", LOG_PATH, ["Message"])
 
-    print('Starting temperature logging')
-    status_log.log(['Starting temperature logging'])
+    print("Starting temperature logging")
+    status_log.log(["Starting temperature logging"])
 
     while True:
         temp = sensor.read_temp()
-        print('Temperature:', temp)
+        print("Temperature:", temp)
         temp_log.log(temp)
         time.sleep(60)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
